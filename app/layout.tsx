63bc7cc4266bdb71367798e48dbd0dd4d7568b8f
DuyNguyen123456 <duyq8451@gmail.com>
2026-06-13 21:28:30 +0700

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
-import "./globals.css"
 import Navbar from "@/components/navbar"
+import { Analytics } from "@vercel/analytics/next"
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "GenD Arena 2026",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body style={{ margin: 0 }}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   )
